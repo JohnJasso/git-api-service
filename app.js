38cafcd0c835233d7c1cd086d0f46b5ff03a3e53
John Jasso <johnjasso@Johns-MacBook-Pro.local>
2020-04-14 12:02:54 -0500

    (Gapi-3) Improves validation.

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const exploiter = require('./exploiter')
+const validation = require('./validation')
+
 const app = express()
 const port = process.env.PORT || 3000;
 
@@ -10,18 +12,16 @@ app.get('/', (req, res) => {
 
 // End-point for searching repositories
 app.get('/api/search-repo', async (req, res) => {
-    if(!req.query) {
-        res.status(400).send('A search query should be included');
-    } else if (!req.query.term) {
-        res.status(400).send("The query should be defined by ?term=");
+    const { error } = validation.validateSearch(req.query);
+    if(error) {
+        return res.status(400).send('A search query should be included and defined as \"?term=[searchquery]\"');
     } else {
         try {
             const response = await exploiter.search(req.query.term);
-            if (!response)
-                res.status(400).send("Not found");
-            else
-                res.status(200).send(response);
-        } catch(error) {
+            if (!response) res.status(400).send("Not found");
+            else res.status(200).send(response);
+        } catch (error) {
+            console.log("ERROR: ", error);
             res.send(error);
         }
     }
@@ -31,53 +31,64 @@ app.get('/api/search-repo', async (req, res) => {
 
 // End-point for listing all bookmarks
 app.get("/api/bookmarks", async (req, res) => {
-  try {
-    const response = await exploiter.list();
-    if(!response)
-        res.status(400).send('Not found');
-    else
-        res.status(200).send(response);
-  } catch (error) {
-      res.send(error);
-  }
+    try {
+        const response = await exploiter.list();
+        if (!response) res.status(400).send("Not found");
+        else res.status(200).send(response);
+    } catch (error) {
+        console.log("ERROR: ", error);
+        res.send(error);
+    }
 });
 
 // End-point for bookmarking a repository
 app.put("/api/bookmark-repo/:id", async (req, res) => {
-  try {
-    const repo = await exploiter.searchID(req.params.id);
-    const response = await exploiter.starAuth(req.params.id);
-    if(!response)
-        res.status(400).send('Not found');
-    else {
-        const body = {
-            message: 'Bookmarked repository',
-            repository: repo
+    const { error } = validation.validateID(req.params);
+    if(error) {
+        return res.status(400).send(error.details[0].message);
+    } else {
+        const repo = await exploiter.searchID(req.params.id);
+        if (!repo.id) {
+          res.status(404).send("Repository Not found");
+        } else {
+          try {
+              await exploiter.starAuth(req.params.id);
+              const body = {
+                  message: "Bookmarked repository",
+                  repository: repo,
+              };
+              res.status(200).send(body);
+          } catch (error) {
+              console.log("ERROR: ", error);
+              res.send(error);
+          }
         }
-        res.status(200).send(body);
     }
-  } catch (error) {
-      res.send(error);
-  }
 });
 
 // End-point for removing a bookmark from a repository
 app.delete("/api/bookmark-remove/:id", async (req, res) => {
-  try {
-    const repo = await exploiter.searchID(req.params.id);
-    const response = await exploiter.remStarAuth(req.params.id);
-    if (!response)
-        res.status(400).send("Not found");
-    else {
-        const body = {
-          message: "Removed bookmark",
-          repository: repo,
-        };
-        res.status(200).send(body);
+    const { error } = validation.validateID(req.params);
+    if(error) {
+        return res.status(400).send(error.details[0].message);
+    } else {
+        const repo = await exploiter.searchID(req.params.id);
+        if (!repo.id) {
+          res.status(404).send("Repository Not found");
+        } else {
+          try {
+              await exploiter.remStarAuth(req.params.id);
+              const body = {
+                  message: "Removed bookmark",
+                  repository: repo,
+              };
+              res.status(200).send(body);
+          } catch (error) {
+              console.log("ERROR: ", error);
+              res.send(error);
+          }
+        }
     }
-  } catch (error) {
-      res.send(error);
-  }
 });
 
 app.listen(port, () => {
