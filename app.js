cebc09e731da8d4ecd37f9195fb00a660ec833ea
John Jasso <johnjasso@Johns-MacBook-Pro.local>
2020-04-14 03:12:46 -0500

    (Gapi-2) Creta EP for searching repositories. Create EP for listing bookmarks.

--- app.js
+++ app.js
@@ -1,9 +1,41 @@
 const express = require('express')
 const exploiter = require('./exploiter')
 const app = express()
+const port = process.env.PORT || 3000;
 
-// app.listen(3000, () => {
-//     console.log('Listening on port 3000...');
-// })
+// API initial point
+app.get('/', (req, res) => {
+    res.send('John Jasso\'s GitHub API');
+});
 
-exploiter.search('tetris');
+// End-point for searching repositories
+app.get('/api/search-repo', async (req, res) => {
+    try {
+        const response = await exploiter.search(req.query.term);
+        res.send(response);
+    } catch(error) {
+        res.send(error);
+    }
+    // console.log(req.query);
+    // res.send(req.query);
+})
+
+// End-point for listing all bookmarks
+app.get("/api/bookmarks", async (req, res) => {
+  try {
+    const response = await exploiter.list();
+    res.send(response);
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}...`);
+})
+
+// exploiter.search('tetris');
+// exploiter.list();
+// exploiter.starAuth(178619111);
+// exploiter.listAuth();
+// exploiter.remStarAuth(76954504);
