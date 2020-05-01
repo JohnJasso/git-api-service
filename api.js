ad23c8859bbdba8e3183db2c41dbbdb282ba5bf4
John Jasso <johnjasso@Johns-MacBook-Pro.local>
2020-05-01 14:19:57 -0500

    (develop) Includes CORS. Handles Git API rate limit exceeded.

--- api.js
+++ api.js
@@ -1,52 +1,61 @@
 "use strict";
-const express = require('express');
+const express = require("express");
 const exploiter = require("./exploiter");
 const validation = require("./validation");
 const router = express.Router();
 
 router.use(function (req, res, next) {
-    const date = new Date();
-    console.log(req.method, req.url, "@", date.toLocaleString());
-    next();
+  const date = new Date();
+  console.log(req.method, req.url, "@", date.toLocaleString());
+  next();
 });
 
 // End-point for searching repositories
-router.route("/search-repo")
-    .get(async (req, res) => {
-        const { error } = validation.validateSearch(req.query);
-        if (error) {
-          return res
-            .status(400)
-            .send(
-              'A search query should be included and defined as "?term=[searchquery]"'
-            );
-        } else {
-          const query = req.query.term.replace(/ /g, '+');
-          try {
-            const response = await exploiter.search(query);
-            if (!response) res.status(400).send("Not found");
-            else res.status(200).send(response);
-          } catch (error) {
-            console.log("ERROR: ", error);
-            res.send(error);
-          }
-        }
-    });
+router.route("/search-repo").get(async (req, res) => {
+  const { error } = validation.validateSearch(req.query);
+  if (error) {
+    return res
+      .status(400)
+      .send(
+        'A search query should be included and defined as "?term=[searchquery]"'
+      );
+  } else {
+    const query = req.query.term.replace(/ /g, "+");
+    try {
+      const response = await exploiter.search(query);
+      if (!response) res.status(400).send("Not found");
+      else if (
+        response.message &&
+        response.message.includes("API rate limit exceeded")
+      ) {
+        res.status(403).send("Git API rate limit exceeded");
+      } else res.status(200).send(response);
+    } catch (error) {
+      console.log("ERROR: ", error);
+      res.send(error);
+    }
+  }
+});
 
 // End-point for listing all bookmarks
-router.route("/bookmarks")
-    .get(async (req, res) => {
-        try {
-          const response = await exploiter.list();
-          if (!response) res.status(400).send("Not found");
-          else res.status(200).send(response);
-        } catch (error) {
-          console.log("ERROR: ", error);
-          res.send(error);
-        }
-    });
+router.route("/bookmarks").get(async (req, res) => {
+  try {
+    const response = await exploiter.listAuth();
+    if (!response) res.status(400).send("Not found");
+    else if (
+      response.message &&
+      response.message.includes("API rate limit exceeded")
+    ) {
+      res.status(403).send("Git API rate limit exceeded");
+    } else res.status(200).send(response);
+  } catch (error) {
+    console.log("ERROR: ", error);
+    res.send(error);
+  }
+});
 
-router.route("/bookmark-repo/:id")
+router
+  .route("/bookmark-repo/:id")
   // End-point for bookmarking a repository
   .put(async (req, res) => {
     const { error } = validation.validateID(req.params);
@@ -55,15 +64,26 @@ router.route("/bookmark-repo/:id")
     } else {
       const repo = await exploiter.searchID(req.params.id);
       if (!repo.id) {
-        res.status(404).send("Repository Not found");
+        if (repo.message && repo.message.includes("API rate limit exceeded")) {
+          res.status(403).send("Git API rate limit exceeded");
+        } else {
+          res.status(404).send("Repository Not found");
+        }
       } else {
         try {
-          await exploiter.starAuth(req.params.id);
+          const response = await exploiter.starAuth(req.params.id);
           const body = {
             message: "Bookmarked repository",
-            repository: repo,
+            items: await exploiter.listAuth(),
           };
-          res.status(200).send(body);
+          if (
+            response.message &&
+            response.message.includes("API rate limit exceeded")
+          ) {
+            res.status(403).send("Git API rate limit exceeded");
+          } else {
+            res.status(200).send(body);
+          }
         } catch (error) {
           console.log("ERROR: ", error);
           res.send(error);
@@ -79,15 +99,26 @@ router.route("/bookmark-repo/:id")
     } else {
       const repo = await exploiter.searchID(req.params.id);
       if (!repo.id) {
-        res.status(404).send("Repository Not found");
+        if (repo.message && repo.message.includes("API rate limit exceeded")) {
+          res.status(403).send("Git API rate limit exceeded");
+        } else {
+          res.status(404).send("Repository Not found");
+        }
       } else {
         try {
-          await exploiter.remStarAuth(req.params.id);
+          const response = await exploiter.remStarAuth(req.params.id);
           const body = {
             message: "Removed bookmark",
-            repository: repo,
+            items: await exploiter.listAuth(),
           };
-          res.status(200).send(body);
+          if (
+            response.message &&
+            response.message.includes("API rate limit exceeded")
+          ) {
+            res.status(403).send("Git API rate limit exceeded");
+          } else {
+            res.status(200).send(body);
+          }
         } catch (error) {
           console.log("ERROR: ", error);
           res.send(error);
--- app.js
+++ app.js
@@ -1,19 +1,21 @@
-const express = require('express');
-const api = require('./api');
+const express = require("express");
+const cors = require("cors");
+const api = require("./api");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use("/api", api);
 const port = process.env.PORT || 3000;
 
 // API initial point
-app.get('/', (req, res) => {
-    res.send('John Jasso\'s GitHub API');
+app.get("/", (req, res) => {
+  res.send("John Jasso's GitHub API");
 });
 
-app.listen(port, err => {
-    if(err) {
-        return console.log('ERROR: ', err);
-    }
-    console.log(`Listening on port ${port}...`);
+app.listen(port, (err) => {
+  if (err) {
+    return console.log("ERROR: ", err);
+  }
+  console.log(`CORS-enabled web server listening on port ${port}...`);
 });
